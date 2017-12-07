2a0356663250dcbf70a8120d08133077bf5e2dc0
manchiyo <34336586+manchiyo@users.noreply.github.com>
2017-12-08 02:32:34 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+client.on('message', message => {
+    if (message.content === 'Ai-chan') {
+    	message.reply('nandeshou?');    
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
